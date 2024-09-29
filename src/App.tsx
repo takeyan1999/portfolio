7e71bd3b26a6a97f1a572db5a4cab310978f85c7
takeyan1999 <take10.tennis@gmail.com>
2024-09-29 16:26:10 +0900

    animation

--- src/App.tsx
+++ src/App.tsx
@@ -26,19 +26,27 @@ const App = () => {
                 <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
                     <Container>
                         <Navbar.Brand href="#home">
-                            <Scroll to="home">Run's Folio</Scroll>
+                            <Scroll to="home" smooth={true} duration={200}>
+                                Run's Folio
+                            </Scroll>
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
                                 <Nav.Link href="#concept">
-                                    <Scroll to="concept">Concept</Scroll>
+                                    <Scroll to="concept" smooth={true} duration={200}>
+                                        Concept
+                                    </Scroll>
                                 </Nav.Link>
                                 <Nav.Link href="#Works">
-                                    <Scroll to="works">Works</Scroll>
+                                    <Scroll to="works" smooth={true} duration={200}>
+                                        Works
+                                    </Scroll>
                                 </Nav.Link>
                                 <Nav.Link href="#about">
-                                    <Scroll to="about">About</Scroll>
+                                    <Scroll to="about" smooth={true} duration={200}>
+                                        About
+                                    </Scroll>
                                 </Nav.Link>
                             </Nav>
                         </Navbar.Collapse>
@@ -46,77 +54,79 @@ const App = () => {
                 </Navbar>
             </header>
             <body>
-                <div
-                    className="vh-100 d-flex align-items-center "
-                    style={{
-                        backgroundImage: `url(${Topimg})`,
-                        backgroundSize: "cover",
-                    }}
-                    id="home"
-                >
-                    <div className="headerfont">
-                        <p>TAKERU</p>
-                        <p>IWAKI</p>
-                    </div>
-                </div>
+                <div className="scrollsnap">
+                    <section
+                        className="section d-flex align-items-center "
+                        style={{
+                            backgroundImage: `url(${Topimg})`,
+                            backgroundSize: "cover",
+                        }}
+                        id="home"
+                    >
+                        <div className="headerfont">
+                            <p>TAKERU</p>
+                            <p>IWAKI</p>
+                        </div>
+                    </section>
 
-                <div className="vh-100 content-container keyconcept text-center" id="concept">
-                    <p className="content-title">KEY CONCEPT</p>
-                    <div className="concept-content">
-                        <p>目の前の人にワクワクを</p>
-                        <p>ワクワクで広がる世界</p>
-                    </div>
-                </div>
-                <div className="content-container mywork text-center" id="works">
-                    <p className="content-title">MY WORK</p>
-                    <Container className="">
-                        <Row>
-                            {works.map(({ id, url, picture }) => {
-                                return <Mywork id={id} url={url} picture={picture} />;
-                            })}
-                        </Row>
-                    </Container>
-                </div>
-                <div className="vh-100 content-container keyconcept text-center" id="about">
-                    <p className="content-title">ABOUT ME</p>
-                    <Container>
-                        <Row>
-                            <Col className="d-flex justify-content-center">
-                                <div
-                                    className="picturebox"
-                                    style={{
-                                        backgroundImage: `url(${image})`,
-                                        backgroundSize: "220%",
-                                        backgroundRepeat: "no-repeat",
-                                        backgroundPosition: "0px -80px",
-                                    }}
-                                ></div>
-                            </Col>
-                            <Col>
-                                <div>
-                                    <p className="section-title">name</p>
-                                    <p className="content-text">
-                                        <ruby>
-                                            岩木<rt>いわき</rt>&nbsp;健讓<rt>たける</rt>
-                                        </ruby>
-                                    </p>
-                                    <p className="section-title">career</p>
+                    <section className="section content-container keyconcept text-center" id="concept">
+                        <p className="content-title">KEY CONCEPT</p>
+                        <div className="concept-content">
+                            <p>目の前の人にワクワクを</p>
+                            <p>ワクワクで広がる世界</p>
+                        </div>
+                    </section>
+                    <section className="section content-container mywork text-center" id="works">
+                        <p className="content-title">MY WORK</p>
+                        <Container className="">
+                            <Row>
+                                {works.map(({ id, url, picture }) => {
+                                    return <Mywork id={id} url={url} picture={picture} />;
+                                })}
+                            </Row>
+                        </Container>
+                    </section>
+                    <section className="section content-container keyconcept text-center" id="about">
+                        <p className="content-title">ABOUT ME</p>
+                        <Container>
+                            <Row>
+                                <Col className="d-flex justify-content-center">
+                                    <div
+                                        className="picturebox"
+                                        style={{
+                                            backgroundImage: `url(${image})`,
+                                            backgroundSize: "220%",
+                                            backgroundRepeat: "no-repeat",
+                                            backgroundPosition: "0px -80px",
+                                        }}
+                                    ></div>
+                                </Col>
+                                <Col>
+                                    <div>
+                                        <p className="section-title">name</p>
+                                        <p className="content-text">
+                                            <ruby>
+                                                岩木<rt>いわき</rt>&nbsp;健讓<rt>たける</rt>
+                                            </ruby>
+                                        </p>
+                                        <p className="section-title">career</p>
 
-                                    <div className="container text-center">
-                                        <div className="row row-cols-2">
-                                            <div className="col">2018~2022</div>
-                                            <div className="col">神戸大学国際人間科学部</div>
-                                            <div className="col">2022~現在</div>
-                                            <div className="col">公立小学校教員</div>
+                                        <div className="container text-center">
+                                            <div className="row row-cols-2">
+                                                <div className="col">2018~2022</div>
+                                                <div className="col">神戸大学国際人間科学部</div>
+                                                <div className="col">2022~現在</div>
+                                                <div className="col">公立小学校教員</div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Container>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </section>
                 </div>
             </body>
-            <footer className="content-container text-center">
+            <footer className="text-center">
                 <div className="d-flex justify-content-around pt-5">
                     <span className="fotter-content">
                         Run's
