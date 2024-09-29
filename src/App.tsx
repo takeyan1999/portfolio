b02a656ef20865f019ddb877ac113623e99609b0
takeyan1999 <take10.tennis@gmail.com>
2024-09-29 11:15:17 +0900

    design comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link as Scroll } from "react-scroll";
@@ -11,6 +11,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 import Topimg from "./img/top.jpg";
 import quizapp from "./img/quizapp.png";
+import image from "./img/image.jpeg";
 
 const App = () => {
     const works = [
@@ -61,9 +62,9 @@ const App = () => {
 
                 <div className="vh-100 content-container keyconcept text-center" id="concept">
                     <p className="content-title">KEY CONCEPT</p>
-                    <div className="mt-5">
-                        <p className="fs-1">目の前の人にワクワクを</p>
-                        <p className="fs-1">ワクワクで広がる世界</p>
+                    <div className="concept-content">
+                        <p>目の前の人にワクワクを</p>
+                        <p>ワクワクで広がる世界</p>
                     </div>
                 </div>
                 <div className="content-container mywork text-center" id="works">
@@ -78,10 +79,67 @@ const App = () => {
                 </div>
                 <div className="vh-100 content-container keyconcept text-center" id="about">
                     <p className="content-title">ABOUT ME</p>
+                    <Container>
+                        <Row>
+                            <Col className="d-flex justify-content-center">
+                                <div
+                                    className="picturebox"
+                                    style={{
+                                        backgroundImage: `url(${image})`,
+                                        backgroundSize: "220%",
+                                        backgroundRepeat: "no-repeat",
+                                        backgroundPosition: "0px -80px",
+                                    }}
+                                ></div>
+                            </Col>
+                            <Col>
+                                <div>
+                                    <p className="section-title">name</p>
+                                    <p className="content-text">
+                                        <ruby>
+                                            岩木<rt>いわき</rt>&nbsp;健讓<rt>たける</rt>
+                                        </ruby>
+                                    </p>
+                                    <p className="section-title">career</p>
+
+                                    <div className="container text-center">
+                                        <div className="row row-cols-2">
+                                            <div className="col">2018~2022</div>
+                                            <div className="col">神戸大学国際人間科学部</div>
+                                            <div className="col">2022~現在</div>
+                                            <div className="col">公立小学校教員</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Container>
                 </div>
             </body>
-            <footer className="vh-100 content-container text-center">
-                <p className="content-title">Footer</p>
+            <footer className="content-container text-center">
+                <div className="d-flex justify-content-around pt-5">
+                    <span className="fotter-content">
+                        Run's
+                        <br />
+                        Work
+                    </span>
+                    <div>
+                        <ul className="">
+                            <li className="d-flex">
+                                <Scroll to="home">HOME</Scroll>
+                            </li>
+                            <li className="d-flex">
+                                <Scroll to="concept">CONCEPT</Scroll>
+                            </li>
+                            <li className="d-flex">
+                                <Scroll to="work">WORK</Scroll>
+                            </li>
+                            <li className="d-flex">
+                                <Scroll to="about">ABOUT</Scroll>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </footer>
         </>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
                 use: ["style-loader", "css-loader"],
             },
             {
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpg|jpeg|gif)$/,
                 loader: "file-loader",
                 options: {},
             },
